1175b7729f3e8a6189d798524ef629413b69962d
Japneet Kaur <japneetkaur@Japneets-MacBook-Pro.local>
2022-04-21 22:02:35 -0400

    simple form post method for search bar input with req.body printing working

--- routes/search.js
+++ routes/search.js
@@ -1,9 +1,16 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-/* GET users listing. */
+
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render ('search');
 });
+
+
+router.post ('/', (req, res) => {
+  console.log (req.body);
+  res.send ('responding with a resource');
+});
+
 
 module.exports = router;
